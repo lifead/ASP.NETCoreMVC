594e4b4fb0780773ac55e2765c31a10f7b007f5c
Евгений <lifead@yandex.ru>
2020-03-05 20:57:15 +0700

    Выполнение д.з. 1

---
+++ WebStore/WebStore/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Configuration) => this.Configuration = Configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(); //services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/greetings", async context =>
+                {
+                    await context.Response.WriteAsync(Configuration["CustomGreetings"]);
+                });
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"); 
+            });
+        }
+    }
+}
