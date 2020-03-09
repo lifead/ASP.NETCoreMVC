b8889febb7b63d06d8c2ad6039cd30946263a57e
Evgeniy <12@MyPC>
2020-03-09 17:04:59 +0300

    Добавлена связь с блаузером

--- WebStore/WebStore/Startup.cs
+++ WebStore/WebStore/Startup.cs
@@ -27,6 +27,7 @@ namespace WebStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
             app.UseStaticFiles();
