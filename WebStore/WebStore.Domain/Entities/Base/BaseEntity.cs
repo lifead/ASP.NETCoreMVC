21e40d614789a40d0bed28d99f9055e4e01ecb2d
Евгений <vasilev.jv@gmail.com>
2020-03-15 18:56:56 +0700

    Добавление доменной модели, базовых интерфейсов, базовых абстрактных классов

---
+++ WebStore/WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities.Base
+{
+    /// <summary>
+    /// Базовая сущность
+    /// </summary>
+    public abstract class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
