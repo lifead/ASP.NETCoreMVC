fca0433b78e45aa4f4841457cb4918a22ae9398f
Евгений <vasilev.jv@gmail.com>
2020-03-22 20:39:17 +0700

    Контроллер Account + представление

---
+++ WebStore/WebStore/Controlers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controlers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
