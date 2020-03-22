bd517f6bc1bb50c35fa43fc682ba86740f627213
Евгений <vasilev.jv@gmail.com>
2020-03-22 20:32:03 +0700

    Регистрация сервисов аутентификации

--- WebStore/WebStore/Startup.cs
+++ WebStore/WebStore/Startup.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.DAL.Context;
 using WebStore.Data;
+using WebStore.Domain.Entities.Identity;
 using WebStore.Services;
 
 namespace WebStore
@@ -20,7 +23,7 @@ namespace WebStore
 
            
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
-            
+
             //Добавление и регистрация сервисов данного приложения
             services.AddWebStoreInterfaces(Configuration);
         }
@@ -38,6 +41,8 @@ namespace WebStore
 
             app.UseStaticFiles();
             app.UseDefaultFiles();
+
+            app.UseAuthentication();
 
             app.UseRouting();
 
