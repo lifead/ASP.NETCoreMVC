1346cb3d6dc91e3bf5b7aace3ba4901e72c3c04c
Евгений <vasilev.jv@gmail.com>
2020-03-22 22:29:15 +0700

    Проработка логики регистрации пользователя

--- WebStore/WebStore/Controlers/AccountController.cs
+++ WebStore/WebStore/Controlers/AccountController.cs
@@ -2,23 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controlers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register()
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
         {
-            return View(new RegisterUserViewModel());
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
         }
 
-        public IActionResult Login()
+        #region Регистрация пользователя в системе
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
         {
-            return View(new LoginViewModel());
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var register_result = await _UserManager.CreateAsync(user, Model.Password);
+            if (register_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in register_result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            return View(Model);
         }
 
+        #endregion
 
+        #region Вход пользователя в систему
+
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var login_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+                false);
+
+            if (login_result.Succeeded)
+            {
+                if (Url.IsLocalUrl(Model.ReturnUrl))
+                    return Redirect(Model.ReturnUrl);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Неверное имя пользователя, или пароль");
+
+            return View(Model);
+        }
+
+        #endregion
+
+        public async Task<IActionResult> Logout()
+        {
+            await _SignInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
+
 }
