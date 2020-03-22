f2efe746333744fa3e36221ecd0f0f741559bc15
Евгений <vasilev.jv@gmail.com>
2020-03-22 11:18:46 +0700

    Добавление контроллера

---
+++ WebStore/WebStore/Controlers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Controlers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogData _BlogData;
+
+        public BlogController(IBlogData BlogData) => _BlogData = BlogData;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
