2b6295489f4e914d061ddde0a76c9681ab1f173b
Евгений <vasilev.jv@gmail.com>
2020-03-22 19:17:35 +0700

    Сущность Пользователь и Роль

---
+++ WebStore/WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    /// <summary>
+    /// Пользователь (переопределенная сущность)
+    /// </summary>
+    public class User: IdentityUser
+    {
+        public const string Administrator = "Admin";
+        public const string DefaultPassword = "AdminPassword";
+    }
+}
