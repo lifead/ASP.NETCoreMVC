7d970b6bbd8a063cfe0c21dd8f6b17f349d79731
Евгений <vasilev.jv@gmail.com>
2020-03-29 15:19:03 +0700

    Заготовки под методу/представления для редактирование и удаление товаров

--- WebStore/WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Infrastructure.Interfaces;
 
@@ -13,5 +14,28 @@ namespace WebStore.Areas.Admin.Controllers
         public ProductsController(IProductData ProductData) => _ProductData = ProductData;
 
         public IActionResult Index(/*[FromServices] IProductData Products*/) => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int? id)
+        {
+            var product = id is null ? new Product() : _ProductData.GetProductById((int)id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _ProductData.GetProductById(id);
+
+            if (product is null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction(nameof(Index));
     }
 }
