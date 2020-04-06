78dc870228586e2d407d2424b1d96b035b38c80c
Евгений <vasilev.jv@gmail.com>
2020-04-06 22:42:23 +0700

    Класс базового клиента WebApi

---
+++ WebStore/Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient : IDisposable
+    {
+        protected readonly HttpClient _Client;
+        protected readonly string _ServiceAddress;
+
+        protected BaseClient(IConfiguration Configuration, string ServiceAddress)
+        {
+            _ServiceAddress = ServiceAddress;
+
+            _Client = new HttpClient()
+            {
+                BaseAddress = new Uri(Configuration["WebApiURL"])
+            };
+            _Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        #region IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_Disposed || !disposing) return;
+            _Disposed = true;
+            _Client.Dispose();
+        } 
+        #endregion
+
+    }
+}
