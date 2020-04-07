a0457be1de8e784a93f8e2f6327dfe5f853e1d95
Евгений <vasilev.jv@gmail.com>
2020-04-07 19:04:26 +0700

    Удаление и повторное добавление проекта WebStore.Interfaces - изменение формата библиотеки

---
+++ WebStore/Services/WebStore.Clients/Values/ValuesClient.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Clients.Base;
+using WebStore.Interfaces.Api;
+
+namespace WebStore.Clients.Values
+{
+    class ValuesClient :BaseClient, IValuesService
+    {
+        public ValuesClient(IConfiguration Configuration) : base(Configuration, "api/values"){}
+
+        public HttpStatusCode Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpStatusCode> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Uri Post(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Uri> PostAsync(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpStatusCode Put(int id, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpStatusCode> PutAsync(int id, string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
