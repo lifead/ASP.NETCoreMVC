db460d62935723736a6f6aede1ae1d82c01f6485
Евгений <vasilev.jv@gmail.com>
2020-04-13 16:38:47 +0700

    Заготовка конторллеров WebAPI.Identity

---
+++ WebStore/Services/WebStore.ServiceHosting/Controllers/RolesApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Identity.Roles)]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+    }
+}
---
+++ WebStore/Services/WebStore.ServiceHosting/Controllers/UsersApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Identity.Users)]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+    }
+}
