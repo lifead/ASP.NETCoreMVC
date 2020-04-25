b29983579edef62b9098b91fff000555087931ad
Евгений <vasilev.jv@gmail.com>
2020-04-25 15:06:51 +0700

    Доработан визуальный компонент секций - позволяющий подсвечивать выбранную секцию каталога товаров +Стили для визуализации выбора активной секции

---
+++ WebStore/Common/WebStore.Domain/ViewModels/Product/SectionCompleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.ViewModels.Product;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class SectionCompleteViewModel
+    {
+        public IEnumerable<SectionViewModel> Sections { get; set; }
+
+        public int? CurrentParrentSectionId { get; set; }
+
+        public int? CurrentSectionId { get; set; }
+    }
+}
--- WebStore/UI/WebStore/Components/SectionsViewComponent.cs
+++ WebStore/UI/WebStore/Components/SectionsViewComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.ViewModels;
 using WebStore.Domain.ViewModels.Product;
 using WebStore.Interfaces.Services;
 
@@ -15,15 +16,29 @@ namespace WebStore.Components
         private readonly IProductData _ProductData;
         public SectionsViewComponent(IProductData ProductData) => _ProductData = ProductData;
 
-        public IViewComponentResult Invoke() => View(GetSections());
+        public IViewComponentResult Invoke(string SectionId)
+        {
+            var section_id = int.TryParse(SectionId, out var id) ? id : (int?)null;
+
+            var sections = GetSections(section_id, out var parent_section_id);
+
+            return View(new SectionCompleteViewModel
+            {
+                Sections = sections,
+                CurrentSectionId = section_id,
+                CurrentParrentSectionId = parent_section_id
+            });
+        }
 
 
         /// <summary>
         /// Получить все секции
         /// </summary>
         /// <returns>Перечень секций</returns>
-        private IEnumerable<SectionViewModel> GetSections()
+        private IEnumerable<SectionViewModel> GetSections(int? SectionId, out int? ParentSectionId)
         {
+            ParentSectionId = null;
+
             var sections = _ProductData.GetSections().ToArray();
 
             var parent_sections = sections.Where(s => s.ParentId is null);
@@ -42,19 +57,25 @@ namespace WebStore.Components
                 var childs = sections.Where(s => s.ParentId == parent_section.Id);
 
                 foreach (var child_section in childs)
-                    parent_section.ChildSections.Add(new SectionViewModel
-                    {
-                        Id = child_section.Id,
-                        Name = child_section.Name,
-                        Order = child_section.Order,
-                        ParentSection = parent_section
-                    });
+                {
+                    if (child_section.Id == SectionId)
+                        ParentSectionId = parent_section.Id;
+
+                    parent_section.ChildSections.Add(
+                        new SectionViewModel
+                        {
+                            Id = child_section.Id,
+                            Name = child_section.Name,
+                            Order = child_section.Order,
+                            ParentSection = parent_section
+                        });
+                }
 
                 parent_section.ChildSections.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
             }
-
             parent_sections_views.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
             return parent_sections_views;
         }
     }
 }
+
