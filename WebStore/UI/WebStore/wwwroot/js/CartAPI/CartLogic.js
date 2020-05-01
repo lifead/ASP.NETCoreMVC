1362970d14c14a7ad05803c2edd341596732a9cf
Евгений <vasilev.jv@gmail.com>
2020-05-01 15:12:54 +0700

    Заготовка для второй части JS

--- WebStore/UI/WebStore/wwwroot/js/CartAPI/CartLogic.js
+++ WebStore/UI/WebStore/wwwroot/js/CartAPI/CartLogic.js
@@ -1,12 +1,20 @@
 ﻿let Cart = {
     _properties: {
         getCartViewLink: "",
-        addToCartLink: ""
+        addToCartLink: "",
+        decrementLink: "",
+        removeFromCartLink: "",
     },
     init: function (properties) {
         $.extend(Cart._properties, properties);
-
+        Cart.initEvents();
+    },
+    initEvents: function () {
         $(".add-to-cart").click(Cart.addToCart);
+        $(".cart_quantity_up").click(Cart.incremantItem);
+        $(".cart_quantity_down").click(Cart.decrementItem);
+        $(".cart_quantity_delete").click(Cart.removeFromCart);
+        
     },
     addToCart: function (event) {
         event.preventDefault();
@@ -22,12 +30,14 @@
             })
             .fail(function () { console.log(`addToCart fail ${id}`) });
     },
+
     showToolTip: function (button) {
         button.tooltip({ title: 'Добавлено в корзину' }).tooltip('show');
         setTimeout(function () {
             button.tooltip('destroy');
         }, 500);
     },
+
     refreshCartView: function () {
         let container = $('#cart-container');
         $.get(Cart._properties.getCartViewLink)
@@ -35,5 +45,26 @@
                 container.html(cartHtml);
             })
             .fail(function () { console.log(`refreshCartView fail ${id}`) });
-    }
+    },
+
+    incremantItem: function (event) {
+        event.preventDefault();
+
+        let button = $(this);
+        const id = button.data('id'); // data-id="..."
+    },
+
+    decrementItem: function (event) {
+        event.preventDefault();
+
+        let button = $(this);
+        const id = button.data('id'); // data-id="..."
+    },
+
+    removeFromCart: function (event) {
+        event.preventDefault();
+
+        let button = $(this);
+        const id = button.data('id'); // data-id="..."
+    },
 }
